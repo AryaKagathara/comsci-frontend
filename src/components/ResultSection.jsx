f7ac5582e5bf1a9b5eb8f0e6bf981b90b8644c1c
kaushalw3nuts <kaushal.w3nuts@gmail.com>
2023-12-08 12:26:18 +0530

    design changes

--- src/components/ResultSection.jsx
+++ src/components/ResultSection.jsx
@@ -1,7 +1,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import ContentBox from "@/components/layout/ContentBox";
-import CountUp from 'react-countup';
+
 
 
 
@@ -11,25 +11,25 @@ const ResultSection = () => {
 			<div className="result">
 				<div className="container">
 					<div className="result_wrap">
-						<div className="result_content fadeInUp">
+						<div className="result_content">
 							<ContentBox title="Our results in numbers" />
 						</div>
 						<div className="result_detail">
 							<div className="result_textbox">
-								<CountUp start={0} end={99} duration={15} suffix="%" />
-								<h5>Customer satisfaction</h5>
+								<h5>99%</h5>
+								<span>Customer satisfaction</span>
 							</div>
 							<div className="result_textbox">
-								<CountUp start={0} end={32} duration={15} suffix="M" />
-								<h5>Active users</h5>
+								<h5>32M</h5>
+								<span>Active users</span>
 							</div>
 							<div className="result_textbox">
-								<CountUp start={0} end={51} duration={15} suffix="+" />
-								<h5>Team members</h5>
+								<h5>51+</h5>
+								<span>Team members</span>
 							</div>
 							<div className="result_textbox">
-								<CountUp start={0} end={240} duration={15} suffix="%" />
-								<h5>Company growth</h5>
+								<h5>240%</h5>
+								<span>Company growth</span>
 							</div>
 						</div>
 					</div>
