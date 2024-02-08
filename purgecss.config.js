a41fb487595dbd76939c0508f80c609fc4ee5cc9
Arya <arya@Aryas-MacBook-Pro.local>
2024-02-08 15:14:45 +0530

    Removed Unnecessary Libraries - by Arya

--- purgecss.config.js
+++
@@ -1,9 +0,0 @@
-// purgecss.config.js
-module.exports = {
-    content: [
-      './pages/**/*.{js,ts,jsx,tsx}',
-      './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    // Add any other settings you need
-  };
-  
